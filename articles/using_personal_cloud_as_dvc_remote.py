53211eb6c9c9b81480856f15f75f7ed4f8ebf2c4
geovicco-dev <geovicco.dev@gmail.com>
2024-07-17 21:02:33 +1000

    modified articles/using_personal_cloud_as_dvc_remote.py

--- articles/using_personal_cloud_as_dvc_remote.py
+++ articles/using_personal_cloud_as_dvc_remote.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-st.set_page_config(page_title='FileCloud as DVC Store', layout='wide')
+st.set_page_config(page_title='FileCloud as DVC Store')#, layout='wide')
 st.title('Using Personal Cloud as DVC Remote')
 
 st.markdown(
-"""
+f"""
 
 From a data science perspective, versioning code, and it's underlying data and models, is crucial in machine learning projects and in line with [MLOps principles](https://ml-ops.org/content/mlops-principles). By versioning all three parts of our workflow, we ensure reproducibility, facilitate collaboration, and maintain an audit trail of our project's evolution.
 
@@ -35,9 +35,11 @@ The objective is to configure a DVC remote storage that is hosted on our persona
 
 ##### Register and Download
 First step is to register and download [FileCloud Community Edition](https://ce.filecloud.com/#communityTrial). It comes with a 1 year license that apparently is renewable indefinitely. Navigate to the [portal link](https://portal.getfilecloud.com) after signing up and email verification. Use the `Download Now` button and follow the next steps to download the license file.
+""")
 
-![Image](../images/Pasted%20image%2020240705161417.png)
+st.image('images/Pasted image 20240705161417.png')
 
+st.markdown("""
 ##### Install
 At the time of install I was working on Ubuntu 22.04.2 LTS and the following instructions were used:
 ```shell
@@ -60,11 +62,18 @@ After installation, we will configure our admin dashboard. Follow the getting st
 
 If things have gone right, typing `localhost/admin` inside a web browser will land you to the admin login screen (see below). Similarly, typing `localhost/user` redirects you to the user login screen.
 > Admin Credentials - `admin` | `password`
-![[Pasted image 20240705163646.png]]
+"""
+)
+st.image('images/Pasted image 20240705163646.png')
+
+st.markdown("""
 ##### Validate License
 After logging in as an admin, we will be prompted to provide a license file.
-![[Pasted image 20240705164004.png]]
+"""
+)
+st.image('images/Pasted image 20240705164004.png')
 
+st.markdown("""
 ##### Update Storage Path
 Next, we want to edit the storage path where we want FileCloud to save and retrieve data from. Go to `Settings` > `Storage` > `My Files` and edit `Storage Path` field. 
 > For my purpose, I am using `/mnt/d/FileCloud` pointing to a folder called `FileCloud` inside my D drive. 
@@ -75,86 +84,79 @@ Click the `Check Path` to verify if the path is valid and then save changes.
 Next we will create a user called `test-dvc` that will be able to access the cloud storage using it's credentials specified while creating.
 
 Go to `Users` > `+ Add User` > `Create` 
-![[Pasted image 20240705165639.png]]
+"""
+)
 
+st.image('images/Pasted image 20240705165639.png')
+
+st.markdown("""
 ### 2. Configure Localhost and Cloudflare Tunnel 
 [Download and install `Cloudflared`](https://developers.cloudflare.com/cloudflare-one/connections/connect-networks/downloads/)
 
 Create a Temporary Tunnel
 ```shell
 cloudflared tunnel --url http://localhost
-``` 
-![[Pasted image 20240705180148.png]]
+```
+"""
+)
+ 
+st.image('images/Pasted image 20240705180148.png') # To redact
+
+st.markdown("""
 A temporary tunnel is now available via a URL - see the same login screen as our localhost.
-![[Pasted image 20240705180436.png]]
-However, this is only temporary in the sense that once the terminal window running the `cloudflared tunnel` command is closed, this URL will no longer be valid. As cool as this is, it still doesn't provide a permanent cloud storage we are after.
+""")
+
+st.image('images/Pasted image 20240705180436.png')
+
+st.markdown("""However, this is only temporary in the sense that once the terminal window running the `cloudflared tunnel` command is closed, this URL will no longer be valid. As cool as this is, it still doesn't provide a permanent cloud storage we are after.
 
 To create a permanent tunnel, the steps are a bit more involved but straightforward enough and can be followed from multiple online resources as well as the [official documentation](https://developers.cloudflare.com/cloudflare-one/connections/connect-networks/get-started/create-remote-tunnel/).
 
 Based on the instructions outlined in the official documentation, I was able to create a permanent tunnel that points my localhost to a subdomain on a publicly hosted domain name so that whenever I or someone who has the credentials to access the webpage `https://dvcstore.geovicco.xyz` will see the same login screen as above. As long as I, as an admin have provided the user with the right level of access, they will be able to use their credentials to login and start adding and deleting files like they would with their OneDrive or GoogleDrive.
-![[Pasted image 20240705182038.png]]
+""")
+
+st.image('images/Pasted image 20240705182038.png')
+
+st.markdown("""
 Finally, now with a permanent web address pointing to our cloud storage, we can edit the `Server URL` insde our FileCloud admin settings to point to [login page](https://dvcstore.geovicco.xyz).
-![[Pasted image 20240705182650.png]]
+""")
+
+st.image('images/Pasted image 20240705182650.png')
+
+st.markdown("""
 Finally, we can login using the user credentials created earlier. The login screen looks like the image below. We can there is 1TB of storage assigned to this user (configurable inside admin dashboard). Before proceeding to the next section we will install desktop apps as shown on the top right area of the login screen.
-![[Pasted image 20240706001920.png]]
-	The only app required to mount the cloud as a DVC remote is [FileCloud Drive](https://www.filecloud.com/additional-downloads/#drive). Enter the `Server URL` and user account credentials.
-![[Pasted image 20240706004003.png]]
+""")
+
+st.image('images/Pasted image 20240706001920.png')
+
+st.markdown("""
+The only app required to mount the cloud as a DVC remote is [FileCloud Drive](https://www.filecloud.com/additional-downloads/#drive). Enter the `Server URL` and user account credentials.
+ """)
+
+st.image('images/Pasted image 20240706004003.png')
+
+st.markdown("""
 For `3. General Settings` make sure to take note of the mount point. This mount is where all your cloud data will be stored. If the admin has given this user full access, this drive can be used to not only read data but also write data - which is what we want when pushing and pulling our datasets and models using DVC.
-![[Pasted image 20240706004130.png]]
+""")
+
+st.image('images/Pasted image 20240706004130.png')
+
+st.markdown("""
 ### 3. Configure cloud storage as dvc remote
 
-In last part of this already very long article, we will bring everything together. The cloud storage can be mounted as a local driver on our computer and configured as a dvc remote. We will use this remote for versioning a fairly large dataset and a deep learning model trained from it. We are so close to our goal, so please stick around! 
+In last part of this already very long article, we will bring everything together. The cloud storage can be mounted as a local drive on our computer and configured as a `dvc remote`. We will use this remote for versioning a fairly large dataset and a deep learning model trained from it. We are so close to our goal, so please stick around! 
 
-Create Project Folder and Setup Virtual Environment
+Clone Project Repository
 ```bash
-mkdir -p dvc-filecloud-demo && cd dvc-filecloud-demo 
+git clone https://github.com/geovicco-dev/OpenEarthMap.git
+```
+Install Virtual Environment and Activate
+```bash 
 pip install uv && uv venv --python=3.10 .venv && source ./.venv/bin/activate
-```
-Initialise Git Repository and Create Project Files
-```shell
-git init .
-mkdir -p data
-touch .gitignore readme.md  app.py params.yaml requirements.txt notebook.ipynb
-```
-Edit `requirements.txt` 
-```text
-pandas==2.2.2
-nbconvert==7.16.4
-ipykernel==6.29.4
-jupyter==1.0.0
-ipywidgets==8.1.3
-matplotlib==3.9.0
-rioxarray==0.15.0
-dvc==3.51.2
-ensure==1.0.4
-python-box==7.1.1
-zenodo-get==1.6.1
-scikit-learn==1.5.1
-albumentations==1.4.8
-joblib==1.4.2
-numpy==1.26.4
-PyYAML==6.0.1
-segmentation_models_pytorch==0.3.3
-setuptools==68.2.2
-torch==2.3.0
-lightning==2.2.5
-tensorboardx==2.6.2.2
-torch-tb-profiler==0.4.3
-onnx==1.16.1
 ```
 Install dependencies
 ```bash
 uv pip install -r requirements.txt
-```
-
-Before using DVC, lets create a remote repository using GitHub's CLI tool [`gh`](https://github.com/cli/cli)
-```bash
-gh repo create OpenEarthMap --public -d "Git repository for working with OpenEarthMap landcover classification and segmentation dataset. The purpose is to create a reporoducible workflow for training deep learning models and integrate them with DVC for managing datasets and models versioned on an on-premise cloud storage server using FileCloud."
-```
-![[Pasted image 20240705131043.png]]
-Next, lets specify the above repo as the `git remote` for publishing our code.
-```bash
-git remote add origin https://github.com/geovicco-dev/OpenEarthMap.git
 ```
 
 #### Download Data and Prepare Training Metadata
@@ -165,7 +167,7 @@ The first stage of the workflow will involve the following steps:
 - [ ] Save metadata with different splits as a csv
 
 The entire workflow for this stage is contained inside the `src/data_ingestion.py` file. The following parameters inside `params.yaml` are used during processing:
-	```
+
 ```json
 doi: 10.5281/zenodo.7223446 # Digital Object Identifier
 out_dir: data
@@ -178,15 +180,28 @@ random_seed: 37
 ```
 
 The workflow uses `doi` of the dataset and downloads it using `zenodo_get` command from the terminal, saves it and extracts it's contents inside `out_dir` directory. The downloaded files are then aggregated into a pandas DataFrame and only those images that have corresponding labels present are kept, the rest are removed to save some space (these are useless for our purpose since they cannot be used during validation or testing). The remaining image/label pairs are then split into different sets using the `train_test_val_split` and `random_seed`. The resulting pandas DataFrames are then combined and finally saved to path specified by `metadata_file` parameter.
+
+The pipeline for fetching data and preparing metadata is executed via `main.py`:
+
+```shell
+python main.py
+```
+
 #### Versioning Data using DVC
 
 DVC's efficient storage mechanism, which only tracks changes between versions of the files being monitored, is particularly beneficial for large datasets like OpenEarthMap. At the end of stage one, the `data` folder has roughly 9.8 GB worth of files. This is where we will start integrating DVC into our workflow.
 ```shell
 erd data -H -L 1 -y inverted
 ```
-![[Pasted image 20240704161634.png]]
-We will start by creating a folder called `dvcstorage` inside the mounted cloud drive.
-![[Pasted image 20240706004713.png]]
+""")
+
+st.image('images/Pasted image 20240704161634.png')
+
+st.markdown("""We will start by creating a folder called `dvcstorage` inside the mounted cloud drive.""")
+
+st.image('images/Pasted image 20240706004713.png')
+
+st.markdown("""
 ##### Initialise DVC
 ```shell
 dvc init
@@ -204,14 +219,28 @@ dvc remote add --default filecloud ~/cloudmount/My\ Files/dvcstorage
 ```shell
 dvc push -r filecloud
 ```
-![[Pasted image 20240706005747.png]]
+""")
+
+st.image('images/Pasted image 20240706005747.png')
+
+st.markdown("""
 Now when we check our `~/cloudmount` folder, we will see that versioned `data` directory has been successfully pushed to `dvc remote`. 
-![[Pasted image 20240706105726.png]]
+""")
+
+st.image('images/Pasted image 20240706105726.png')
+
+st.markdown("""
 Lets push these changes to git so that next time we do dvc pull followed by git clone or git pull, all contents of the `data` folder are retrieved from the dvc `remote`.
 ```shell
 git add . && git commit -m "added dvc remote and tracked data directory"
 git push -u origin main
 ```
+---
 
+That's it! You have managed to integrate DVC into a typical deep learning workflow. Next time you are on the move, you can just pull the data from the dvc remote and start using it just like it was on your local drive or a cloud drive.
+
+P.S. I have exluded the second stage which involves training a model. Feel free to check the GitHub repository for more details.
+
+Link to GitHub Repository: https://github.com/geovicco-dev/OpenEarthMap
 """
 )
